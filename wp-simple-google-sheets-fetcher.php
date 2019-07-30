cfd4d31f0844eae63ef2027ed45b2e81d1b62c1c
naoki <n.globe.us@gmail.com>
2019-07-30 09:20:14 +0900

    Initial commit

---
+++ wp-simple-google-sheets-fetcher.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name:     Wp Simple Google Sheets Fetcher
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Simple plugin to fetch data from google spreadsheet.
+ * Author:          Naoki Ohashi
+ * Author URI:      https://naoki-is-me
+ * Text Domain:     wp-simple-google-sheets-fetcher
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Simple_Google_Sheets_Fetcher
+ */
+
+// Your code starts here.
+
+//## Prerequisites
+//1. Get API KEY from here -> https://developers.google.com/sheets/api/quickstart/php?hl=ja
