64a2a78df2c9100f2bed589c52222587e1466f59
naoki <n.globe.us@gmail.com>
2019-12-03 23:05:16 +0900

    fix some

--- wp-simple-spreadsheet-fetcher-for-google.php
+++ wp-simple-spreadsheet-fetcher-for-google.php
@@ -12,12 +12,12 @@
  * @package         Wp_Simple_Spreadsheet_Fetcher_for_Google
  */
 
-define( "INC_DIR", '/blocks/inc' );
+define( "BUILD_DIR", '/blocks/build' );
 
 include_once dirname( __FILE__ ) . '/vendor/autoload.php';
-include_once dirname( __FILE__ ) . INC_DIR . '/base.php';
-include_once dirname( __FILE__ ) . INC_DIR . '/index.php';
-include_once dirname( __FILE__ ) . INC_DIR . '/get-value-query.php';
+include_once dirname( __FILE__ ) . BUILD_DIR . '/base.php';
+include_once dirname( __FILE__ ) . BUILD_DIR . '/index.php';
+include_once dirname( __FILE__ ) . BUILD_DIR . '/get-value-query.php';
 
 class WPSimpleSpreadsheetFetcherForGoogle {
 
