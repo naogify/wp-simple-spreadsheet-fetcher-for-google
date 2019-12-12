d6fb4aa7a04f1bac4a5fcc015604e0b7a98ea2f0
naoki <n.globe.us@gmail.com>
2019-12-12 10:21:04 +0900

    add deprecated

--- blocks/src/fetcher-item/fetcher-item.js
+++ blocks/src/fetcher-item/fetcher-item.js
@@ -29,34 +29,55 @@ registerBlockType('wp2s2fg/fetcher-item', {
         sheetRange: {
             type: 'string',
             default: '',
+        },
+        //This attributes is deprecated since v0.2.8.
+        range: {
+            type: 'string',
+            default: '',
         }
     },
 
     edit(props) {
-        const {sheetId, sheetName, sheetRange, attributes, setAttributes} = props;
+        const {attributes, setAttributes} = props;
+        const {range, sheetId, sheetName, sheetRange} = attributes;
+
+        let renderSettings = () => {
+            if (!range) {
+                return <Fragment>
+                    <TextControl
+                        label={__(`Sheet URL`, 'wp2s2fg')}
+                        value={sheetId}
+                        onChange={(newUrl) => setAttributes({sheetId: newUrl === undefined ? 'none' : newUrl})}
+                        initialOpen={true}
+                    />
+                    <TextControl
+                        label={__(`Sheet Name`, 'wp2s2fg')}
+                        value={sheetName}
+                        onChange={(newName) => setAttributes({sheetName: newName === undefined ? 'none' : newName})}
+                        initialOpen={true}
+                    />
+                    <TextControl
+                        label={__(`Range`, 'wp2s2fg')}
+                        value={sheetRange}
+                        onChange={(newRange) => setAttributes({sheetRange: newRange === undefined ? 'none' : newRange})}
+                        initialOpen={true}
+                    />
+                </Fragment>;
+            } else {
+                return <TextControl
+                    label={__(`Please set the range to fetch data in A1 notation. Example : Sheets1!A1:E`, 'wp2s2fg')}
+                    value={range}
+                    onChange={(newRange) => setAttributes({range: newRange === undefined ? 'none' : newRange})}
+                    initialOpen={true}
+                />
+            }
+        };
 
         return (
             <Fragment>
                 <InspectorControls>
                     <PanelBody title={__('Fetch Data Setting', 'wp2s2fg')} initialOpen={true}>
-                        <TextControl
-                            label={__(`Sheet URL`, 'wp2s2fg')}
-                            value={sheetId}
-                            onChange={(newUrl) => setAttributes({sheetId: newUrl === undefined ? 'none' : newUrl})}
-                            initialOpen={true}
-                        />
-                        <TextControl
-                            label={__(`Sheet Name`, 'wp2s2fg')}
-                            value={sheetName}
-                            onChange={(newName) => setAttributes({sheetName: newName === undefined ? 'none' : newName})}
-                            initialOpen={true}
-                        />
-                        <TextControl
-                            label={__(`Range`, 'wp2s2fg')}
-                            value={sheetRange}
-                            onChange={(newRange) => setAttributes({sheetRange: newRange === undefined ? 'none' : newRange})}
-                            initialOpen={true}
-                        />
+                        {renderSettings()}
                     </PanelBody>
                 </InspectorControls>
                 <ServerSideRender
