5326333863d501ad740d744b5ec6d9473be0fadb
naoki <n.globe.us@gmail.com>
2020-01-08 08:51:00 +0900

    Fix: loading translation file for text in javascript from .org

--- wp-simple-spreadsheet-fetcher-for-google.php
+++ wp-simple-spreadsheet-fetcher-for-google.php
@@ -7,10 +7,10 @@
  * Author URI:      https://naoki-is-me
  * Text Domain:     wp-simple-spreadsheet-fetcher-for-google
  * Domain Path:     /languages
- * Version:         0.3.9
+ * Version:         0.4.0
  *
  * @package         Wp_Simple_Spreadsheet_Fetcher_for_Google
- * @version 		0.3.9
+ * @version 		0.4.0
  */
 
 define( "BUILD_DIR", '/blocks/build' );
