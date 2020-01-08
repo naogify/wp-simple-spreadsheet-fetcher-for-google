2c33a1e0a61dfd35435fa4f0429d0e6170cb19e0
naoki <n.globe.us@gmail.com>
2020-01-08 23:16:21 +0900

    Fix:deploy settings

--- bin/create-release.sh
+++ bin/create-release.sh
@@ -10,12 +10,9 @@ fi
 version=$1
 
 if [ ! `echo $version | grep -e 'alpha' -e 'beta' -e 'RC' -e 'rc'` ] ; then
-  sed -i '' -e "s/^Stable tag: .*/Stable tag: ${version}/g" readme.txt;
+  sed -i readme.txt -e "s/^Stable tag: .*/Stable tag: ${version}/g";
 fi
 
-whoami
-ls -l
-
-sed -i '' -e "s/^ \* Version: .*/ * Version: ${version}/g" wp-simple-spreadsheet-fetcher-for-google.php;
-sed -i '' -e "s/^ \* @version .*/ * @version ${version}/g" wp-simple-spreadsheet-fetcher-for-google.php;
-sed -i '' -e "s/\"version\"\: \".....\"/\"version\"\: \"${version}\"/g" package.json;
+sed -i wp-simple-spreadsheet-fetcher-for-google.php -e "s/^ \* Version: .*/ * Version: ${version}/g";
+sed -i wp-simple-spreadsheet-fetcher-for-google.php -e "s/^ \* @version .*/ * @version ${version}/g";
+sed -i package.json -e "s/\"version\"\: \".....\"/\"version\"\: \"${version}\"/g";
