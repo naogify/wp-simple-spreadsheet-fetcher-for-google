d9498804e58213017f2e29810ba0d3d38655cedd
naoki <n.globe.us@gmail.com>
2020-01-10 13:28:21 +0900

    Update package.json and add README.md and use @wordpress module

---
+++ README.md
@@ -0,0 +1,113 @@
+# WP Simple Spreadsheet Fetcher for Google #
+**Contributors:** [naoki0h](https://profiles.wordpress.org/naoki0h)  
+**Tags:** fetcher, api, Google Sheets  
+**Requires at least:** 5.0  
+**Tested up to:** 5.3  
+**Stable tag:** 0.4.8  
+**License:** GPLv3 or later  
+**License URI:** https://www.gnu.org/licenses/gpl-3.0.html  
+
+This is the simple plugin to fetch data from Google Sheets and display it on your website.
+
+## How to use ##
+
+1. Create the api key. For more detail. Please refer to [https://developers.google.com/sheets/api/quickstart/js#step_1_turn_on_the](https://developers.google.com/sheets/api/quickstart/js#step_1_turn_on_the).
+2. Move to the settings page from side menu that Plugins > WP Simple Google Sheets Fetcher.
+3. Save your API key at the settings page.
+4. Turn on Get shareable link. For more detail. Please refer to [https://support.google.com/drive/answer/2494822#link_sharing](https://support.google.com/drive/answer/2494822#link_sharing)
+5. Choose "Fetcher" block at the "WP Simple Spreadsheet Fetcher for Google" category, use side panel to indicate the cell to fetch data.
+6. It will display the data in your post or page.
+
+[youtube https://youtu.be/VYMFFMyRK3I]
+
+## Contact ##
+If you have anything to tell me, please post it on the plugin's support forum or reply or message me on twitter @naogify.
+
+## Installation ##
+
+1. Download the zip, extract it and upload the extracted folder to your-wp-directory/wp-content/plugins/
+2. Go to the plugins administration screen in your WordPress admin and activate the plugin.
+
+## Screenshots ##
+
+1. Fetch and display data from Google Sheets.
+
+## Changelog ##
+
+### 0.4.9 ###
+- Fix version.
+
+### 0.4.8 ###
+- Fix version.
+
+### 0.4.7 ###
+- Fix version.
+
+### 0.4.6 ###
+- Fix version.
+
+### 0.4.2 ###
+- Fix version.
+- Fix develop tool.
+
+### 0.4.1 ###
+- Fix version.
+
+### 0.4.0 ###
+- Fix version.
+
+### 0.3.9 ###
+- Fix version.
+
+### 0.3.7 ###
+- Fix text message in fecher advanced block
+- Fix css.
+- Fix CSRF.
+
+### 0.3.6 ###
+- Add japanese translation for text in javascript.
+
+### 0.3.4 ###
+- Fix text and i18n.
+
+### 0.3.3 ###
+- Fix text and i18n.
+
+### 0.3.2 ###
+- Fix user interface.
+
+### 0.3.1 ###
+- Fix user interface.
+
+### 0.3.0 ###
+- Fix user interface
+- Enable to set sheet id from each fetcher block.
+
+### 0.2.7 ###
+- Bug fix.
+
+### 0.2.6 ###
+- Bug fix.
+
+### 0.2.5 ###
+- Delete parser block, add fetcher and fetcher advanced blocks.
+- Fix some.
+
+### 0.2.4 ###
+Just fix text.
+
+### 0.2.3 ###
+Fix the description, and add event register layout.
+
+### 0.2.0 ###
+Fix to register wordpress.org.
+
+### 0.1.0 ###
+First commit.
+
+## Upgrade Notice ##
+
+### 0.1.0 ###
+First commit.
+
+This plugin has backward compatibility.
--- blocks/src/fetcher/fetcher.js
+++ blocks/src/fetcher/fetcher.js
@@ -1,147 +1,149 @@
-const { __ } = wp.i18n;
-const { registerBlockType } = wp.blocks;
-const { TextControl, PanelBody, ServerSideRender } = wp.components;
-const { Fragment } = wp.element;
-const { RichText, InspectorControls } =
-	wp.blockEditor && wp.blockEditor.BlockEdit ? wp.blockEditor : wp.editor;
+import { __ } from '@wordpress/i18n';
+import { registerBlockType } from '@wordpress/blocks';
+import {
+	TextControl,
+	PanelBody,
+	ServerSideRender,
+} from '@wordpress/components';
+import { Fragment } from '@wordpress/element';
+import { InspectorControls } from '@wordpress/block-editor';
 
-registerBlockType("wp2s2fg/fetcher", {
-	title: __("Fetcher", "wp-simple-spreadsheet-fetcher-for-google"),
-	icon: "smiley",
-	category: "wp2s2fg-blocks-cat",
+registerBlockType( 'wp2s2fg/fetcher', {
+	title: __( 'Fetcher', 'wp-simple-spreadsheet-fetcher-for-google' ),
+	icon: 'smiley',
+	category: 'wp2s2fg-blocks-cat',
 	supports: {
 		className: true,
-		html: false
+		html: false,
 	},
 	styles: [
 		{
-			name: "wp2s2fg-fetcher-default",
-			label: __("Default", "wp-simple-spreadsheet-fetcher-for-google"),
-			isDefault: true
+			name: 'wp2s2fg-fetcher-default',
+			label: __( 'Default', 'wp-simple-spreadsheet-fetcher-for-google' ),
+			isDefault: true,
 		},
 		{
-			name: "wp2s2fg-fetcher-line",
-			label: __("Line", "wp-simple-spreadsheet-fetcher-for-google")
+			name: 'wp2s2fg-fetcher-line',
+			label: __( 'Line', 'wp-simple-spreadsheet-fetcher-for-google' ),
 		},
 		{
-			name: "wp2s2fg-fetcher-none",
-			label: __("None", "wp-simple-spreadsheet-fetcher-for-google")
-		}
+			name: 'wp2s2fg-fetcher-none',
+			label: __( 'None', 'wp-simple-spreadsheet-fetcher-for-google' ),
+		},
 	],
 	attributes: {
 		className: {
-			type: "string",
-			default: ""
+			type: 'string',
+			default: '',
 		},
 		sheetId: {
-			type: "string",
-			default: ""
+			type: 'string',
+			default: '',
 		},
 		sheetName: {
-			type: "string",
-			default: ""
+			type: 'string',
+			default: '',
 		},
 		sheetRange: {
-			type: "string",
-			default: ""
+			type: 'string',
+			default: '',
 		},
 		//This attributes is deprecated since v0.2.8.
 		range: {
-			type: "string",
-			default: ""
-		}
+			type: 'string',
+			default: '',
+		},
 	},
 
-	edit(props) {
+	edit( props ) {
 		const { attributes, setAttributes } = props;
 		const { range, sheetId, sheetName, sheetRange } = attributes;
 
-		let renderSettings = () => {
-			if (!range) {
+		const renderSettings = () => {
+			if ( ! range ) {
 				return (
 					<Fragment>
 						<TextControl
-							label={__(
-								"Sheet URL",
-								"wp-simple-spreadsheet-fetcher-for-google"
-							)}
-							value={sheetId}
-							onChange={newUrl =>
-								setAttributes({
+							label={ __(
+								'Sheet URL',
+								'wp-simple-spreadsheet-fetcher-for-google'
+							) }
+							value={ sheetId }
+							onChange={ ( newUrl ) =>
+								setAttributes( {
 									sheetId:
-										newUrl === undefined ? "none" : newUrl
-								})
+										newUrl === undefined ? 'none' : newUrl,
+								} )
 							}
-							initialOpen={true}
+							initialOpen={ true }
 						/>
 						<TextControl
-							label={__(
-								"Sheet Name",
-								"wp-simple-spreadsheet-fetcher-for-google"
-							)}
-							value={sheetName}
-							onChange={newName =>
-								setAttributes({
+							label={ __(
+								'Sheet Name',
+								'wp-simple-spreadsheet-fetcher-for-google'
+							) }
+							value={ sheetName }
+							onChange={ ( newName ) =>
+								setAttributes( {
 									sheetName:
-										newName === undefined ? "none" : newName
-								})
+										newName === undefined ? 'none' : newName,
+								} )
 							}
-							initialOpen={true}
+							initialOpen={ true }
 						/>
 						<TextControl
-							label={__(
-								"Cell or Range",
-								"wp-simple-spreadsheet-fetcher-for-google"
-							)}
-							value={sheetRange}
-							onChange={newRange =>
-								setAttributes({
+							label={ __(
+								'Cell or Range',
+								'wp-simple-spreadsheet-fetcher-for-google'
+							) }
+							value={ sheetRange }
+							onChange={ ( newRange ) =>
+								setAttributes( {
 									sheetRange:
-										newRange === undefined
-											? "none"
-											: newRange
-								})
+										newRange === undefined ?
+											'none' :
+											newRange,
+								} )
 							}
-							initialOpen={true}
+							initialOpen={ true }
 						/>
 					</Fragment>
 				);
-			} else {
-				return (
-					<TextControl
-						label={__(
-							"Please set the cell or range to fetch data in A1 notation. Example : Sheets1!A1:E",
-							"wp-simple-spreadsheet-fetcher-for-google"
-						)}
-						value={range}
-						onChange={newRange =>
-							setAttributes({
-								range:
-									newRange === undefined ? "none" : newRange
-							})
-						}
-						initialOpen={true}
-					/>
-				);
 			}
+			return (
+				<TextControl
+					label={ __(
+						'Please set the cell or range to fetch data in A1 notation. Example : Sheets1!A1:E',
+						'wp-simple-spreadsheet-fetcher-for-google'
+					) }
+					value={ range }
+					onChange={ ( newRange ) =>
+						setAttributes( {
+							range:
+									newRange === undefined ? 'none' : newRange,
+						} )
+					}
+					initialOpen={ true }
+				/>
+			);
 		};
 
 		return (
 			<Fragment>
 				<InspectorControls>
 					<PanelBody
-						title={__(
-							"Fetch Data Setting",
-							"wp-simple-spreadsheet-fetcher-for-google"
-						)}
-						initialOpen={true}
+						title={ __(
+							'Fetch Data Setting',
+							'wp-simple-spreadsheet-fetcher-for-google'
+						) }
+						initialOpen={ true }
 					>
-						{renderSettings()}
+						{ renderSettings() }
 					</PanelBody>
 				</InspectorControls>
 				<ServerSideRender
 					block="wp2s2fg/fetcher"
-					attributes={attributes}
+					attributes={ attributes }
 				/>
 			</Fragment>
 		);
@@ -149,5 +151,5 @@ registerBlockType("wp2s2fg/fetcher", {
 
 	save() {
 		return null;
-	}
-});
+	},
+} );
