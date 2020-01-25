1d4f7b7391c8e0c840157a1493a179c8936f767e
naoki <n.globe.us@gmail.com>
2020-01-25 16:48:57 +0900

    add hooks to tab

--- higher-order/with-tabbed-inspector/index.js
+++ higher-order/with-tabbed-inspector/index.js
@@ -16,7 +16,6 @@ const withTabbedInspector = () =>
 					const onSelect = tabName => {
 						console.log("Helloooooooooo", tabName);
 					};
-
 					return (
 						<Fragment>
 							<InspectorControls>
@@ -41,28 +40,65 @@ const withTabbedInspector = () =>
 											className: "tab-advanced"
 										}
 									]}
+									initialTabName="setting"
 								>
 									{tab => {
-										let description;
+										const blockName = "fetcher";
+										let hooks;
 										if (tab.name === "setting") {
-											description = (
-												<div>これは設定です</div>
+											hooks = (
+												<Fragment>
+													{applyFilters(
+														`wp-simple-spreadsheet-fetcher-for-google.${blockName}.edit.inspector.layout.before`,
+														null,
+														this.props
+													)}
+													{applyFilters(
+														`wp-simple-spreadsheet-fetcher-for-google.${blockName}.edit.inspector.layout.after`,
+														null,
+														this.props
+													)}
+												</Fragment>
 											);
 										} else if (tab.name === "style") {
-											description = (
-												<div>これはスタイルです</div>
+											hooks = (
+												<Fragment>
+													{applyFilters(
+														`wp-simple-spreadsheet-fetcher-for-google.${blockName}.edit.inspector.style.before`,
+														null,
+														this.props
+													)}
+													{applyFilters(
+														`wp-simple-spreadsheet-fetcher-for-google.${blockName}.edit.inspector.style.after`,
+														null,
+														this.props
+													)}
+												</Fragment>
 											);
 										} else if (tab.name === "advanced") {
-											description = (
-												<div>
-													これはアドバンスドです
-												</div>
+											hooks = (
+												<Fragment>
+													{applyFilters(
+														`wp-simple-spreadsheet-fetcher-for-google.${blockName}.edit.inspector.advanced.before`,
+														null,
+														this.props
+													)}
+													{applyFilters(
+														`wp-simple-spreadsheet-fetcher-for-google.${blockName}.edit.inspector.advanced.after`,
+														null,
+														this.props
+													)}
+												</Fragment>
 											);
 										}
 										return (
 											<Fragment>
-												{/* <p>{tab.title}</p> */}
-												{description}
+												{applyFilters(
+													`wp-simple-spreadsheet-fetcher-for-google.${blockName}.edit.inspector.before`,
+													null,
+													this.props
+												)}
+												{hooks}
 											</Fragment>
 										);
 									}}
@@ -77,93 +113,3 @@ const withTabbedInspector = () =>
 	);
 
 export default withTabbedInspector;
-
-// const withTabbedInspector = (tabs = null) =>
-// 	createHigherOrderComponent(
-// 		WrappedComponent =>
-// 			class extends Component {
-// 				static defaultProps = {
-// 					attributes: {},
-// 					blockName: ""
-// 				};
-
-// 				render() {
-// 					const { blockName } = this.props;
-// 					const blockStyleControls = applyFilters(
-// 						`stackable.${blockName}.edit.inspector.style.block`,
-// 						null,
-// 						this.props
-// 					);
-
-// 					return (
-// 						<Fragment>
-// 							{applyFilters(
-// 								`stackable.${blockName}.edit.inspector.before`,
-// 								null,
-// 								this.props
-// 							)}
-
-// 							<InspectorControls>
-// 								<PanelTabs
-// 									tabs={tabs}
-// 									blockProps={this.props}
-// 								/>
-
-// 								<InspectorPanelControls>
-// 									{applyFilters(
-// 										`stackable.${blockName}.edit.inspector.layout.before`,
-// 										null,
-// 										this.props
-// 									)}
-// 									{applyFilters(
-// 										`stackable.${blockName}.edit.inspector.layout.after`,
-// 										null,
-// 										this.props
-// 									)}
-// 								</InspectorPanelControls>
-
-// 								<InspectorPanelControls tab="style">
-// 									{applyFilters(
-// 										`stackable.${blockName}.edit.inspector.style.before`,
-// 										null,
-// 										this.props
-// 									)}
-// 									{applyFilters(
-// 										`stackable.${blockName}.edit.inspector.style.after`,
-// 										null,
-// 										this.props
-// 									)}
-// 									{blockStyleControls && (
-// 										<div
-// 											className="ugb-panel-controls-separator"
-// 											role="presentation"
-// 										>
-// 											— — —
-// 										</div>
-// 									)}
-// 									{blockStyleControls}
-// 								</InspectorPanelControls>
-
-// 								<InspectorPanelControls tab="advanced">
-// 									{applyFilters(
-// 										`stackable.${blockName}.edit.inspector.advanced.before`,
-// 										null,
-// 										this.props
-// 									)}
-// 									{applyFilters(
-// 										`stackable.${blockName}.edit.inspector.advanced.after`,
-// 										null,
-// 										this.props
-// 									)}
-// 								</InspectorPanelControls>
-// 							</InspectorControls>
-
-// 							<WrappedComponent {...this.props} />
-// 						</Fragment>
-// 					);
-// 				}
-// 			},
-// 		"withTabbedInspector"
-// 	);
-
-// export default withTabbedInspector;
