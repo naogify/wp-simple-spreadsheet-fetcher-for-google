b33c60cf45121ec89ce1b0c4f7117ec60fcc7771
naoki <n.globe.us@gmail.com>
2020-01-29 07:02:47 +0900

    Add example of passing argument to toggle

--- components/toggle-control/index.js
+++ components/toggle-control/index.js
@@ -1,25 +1,5 @@
 const { __ } = wp.i18n;
 const { ToggleControl } = wp.components;
-import { withState } from "@wordpress/compose";
-
-// export default withState({
-// 	hasFixedBackground: false
-// })(({ hasFixedBackground, setState }) => (
-// 	<ToggleControl
-// 		label={__(
-// 			"Fixd Table Width",
-// 			"wp-simple-spreadsheet-fetcher-for-google"
-// 		)}
-// 		help={hasFixedBackground ? "Has fixed width." : "No fixed width."}
-// 		checked={hasFixedBackground}
-// 		onChange={() =>
-// 			setState(state => ({
-// 				hasFixedBackground: !state.hasFixedBackground
-// 			}))
-// 		}
-// 	/>
-// ));
-
 import React, { useState } from "react";
 
 function Counter({ initialFixedBackground, label }) {
