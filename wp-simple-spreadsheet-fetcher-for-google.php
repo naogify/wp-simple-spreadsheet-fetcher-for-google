80c37fc5534260317406a5ce9bd6d2c35d13ec72
naogify <n.globe.us@gmail.com>
2020-04-12 00:38:47 +0000

    Update Version

--- wp-simple-spreadsheet-fetcher-for-google.php
+++ wp-simple-spreadsheet-fetcher-for-google.php
@@ -7,10 +7,10 @@
  * Author URI:      https://naoki-is-me
  * Text Domain:     wp-simple-spreadsheet-fetcher-for-google
  * Domain Path:     /languages
- * Version: 0.6.7.5
+ * Version: 0.6.8
  *
  * @package         Wp_Simple_Spreadsheet_Fetcher_for_Google
- * @version 0.6.7.5
+ * @version 0.6.8
  */
 
 define( "BUILD_DIR", '/blocks/build' );
